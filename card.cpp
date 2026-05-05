be7aad1c2c3a24cb28b177b4a0a5223c6da36b9c
Ryan Sun <ryan_sun@ucsb.edu>
2026-05-04 22:21:11 -0700

    init

--- card.cpp
+++ card.cpp
@@ -1,4 +1,132 @@
 // card.cpp
 // Author: Your name
 // Implementation of the classes defined in card.h
+#include "card.h"
+
+Card::Card(char s, int v){
+    setSuit(s);
+    value = v; 
+}
+
+//non-numerical constrcutor
+Card::Card(char s, char v){
+    setSuit(s);
+    setValue(v); 
+}
+//suit 
+void Card::setSuit(char s){
+    switch(s){
+        case 'c':
+        suit = 0; 
+        break;
+
+        case 'd':
+        suit = 1; 
+        break; 
+
+        case 's':
+        suit = 2; 
+        break; 
+
+        case 'h': 
+        suit = 3;
+        break; 
+    }
+}
+
+char Card::getSuit() const{
+    return suit; 
+}
+
+//value 
+void Card::setValue(int v){
+    value = v; 
+}
+
+void Card::setValue(char v){
+    switch(v){
+        case 'a':
+        value = 1; 
+        break; 
+
+        case 'j':
+        value = 11; 
+        break;
+
+        case 'q':
+        value = 12; 
+        break;
+
+        case 'k':
+        value = 13; 
+        break;
+    }
+}
+
+//might run into future problems when doing stuff with ace, 
+//jack, queen, king :)
+int Card::getValue() const{
+    return value; 
+}
+
+//comparators 
+bool Card::operator==(const Card& other) const{
+    return suit == other.suit && value == other.value; 
+}
+
+bool Card::operator<(const Card& other) const{
+    if (suit == other.suit) return value < other.value; 
+    return suit < other.suit; 
+}
+
+bool Card::operator>(const Card& other) const{
+    if (suit == other.suit) return value > other.value; 
+    return suit > other.suit;  
+}
+
+//integer to char helpers
+char suitTranslator(int num){
+    switch(num){
+        case 0:
+        return 'c'; 
+
+        case 1:
+        return 'd'; 
+
+        case 2: 
+        return 's';
+
+        default:
+        return 'h'; 
+    }
+}
+char valueTranslator(int num){
+    switch(num){
+        case 1: 
+        return 'a'; 
+
+        case 11: 
+        return 'j'; 
+
+        case 12: 
+        return 'q';
+
+        default: 
+        return 'k';
+    }
+}
+std::ostream& operator<<(std::ostream& os, const Card& card){
+    char outputSuit = suitTranslator(card.getSuit());
+    char outputValue = ' '; //for ace, jack, etc. 
+    int value = card.getValue();
+
+    if (value == 1 || value > 10){
+        outputValue = valueTranslator(value); 
+        os << outputSuit << " " << outputValue; 
+        return os; 
+    }
+
+    os << outputSuit << " " << value; 
+    return os; 
+}
 
--- card.h
+++ card.h
@@ -1,8 +1,35 @@
 // card.h
-// Author: Your name
+// Author: 
 // All class declarations related to defining a single card go here
 
 #ifndef CARD_H
 #define CARD_H
+#include <ostream> 
+
+class Card{
+public: 
+    Card(char s, int v);
+    Card(char s, char v); 
+
+    void setSuit(char s);
+    char getSuit() const;
+
+    void setValue(int v);
+    int getValue() const;
+
+    //non-numerical values 
+    void setValue(char v);
+    
+    //operators
+    bool operator==(const Card& other) const;
+    bool operator<(const Card& other) const; 
+    bool operator>(const Card& other) const; 
+
+private: 
+    int suit = 0;
+    int value = 0; 
+};
+
+std::ostream& operator<<(std::ostream& os, const Card& card);
 
 #endif
