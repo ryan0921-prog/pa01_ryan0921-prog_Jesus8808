a260fb78e9cfb3edfc09bc5cfaf58cb7ce4585ea
Jesus Santibanez <jsantibanezcastillo@ucsb.edu>
2026-05-06 18:16:32 -0700

    Added struct cardnode

--- card_list.h
+++ card_list.h
@@ -4,5 +4,19 @@
 
 #ifndef CARD_LIST_H
 #define CARD_LIST_H
+#include <iostream>
+#include <string>
+#include "card.h"
+//creating our cardnode structure for our Binrary Search Tree
+struct CardNode {
+    Card data;  //hold data
+    CardNode* left;  //left child poiinter
+    CardNode* right; //right child pointer
+    CardNode* parent; //parent pointer
+
+    CardNode(const Card& card) : data(card), left(nullptr), right(nullptr), parent(nullptr) {}
+
+
+}
 
 #endif
